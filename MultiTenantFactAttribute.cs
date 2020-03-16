89fb99fb40fb9c07fa6f6f13e78e466715a17a57
Ismail Yagoub <ismail.soliman788@gmail.com>
2020-03-16 19:24:29 +0100

    Add files via upload

---
+++ MultiTenantFactAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace FoodAdvisor.Tests
+{
+    public sealed class MultiTenantFactAttribute : FactAttribute
+    {
+        public MultiTenantFactAttribute()
+        {
+            if (!FoodAdvisorConsts.MultiTenancyEnabled)
+            {
+                Skip = "MultiTenancy is disabled.";
+            }
+        }
+    }
+}
